c46cc3e05a7e505d136cb82137a58a49026546fa
kezhengjie <kzj961014@qq.com>
2021-10-20 22:31:34 +0800

    daily update

--- socks5/client_test.go
+++ socks5/client_test.go
@@ -2,9 +2,30 @@ package socks5
 
 import (
 	"testing"
+	"unsafe"
+
+	"github.com/AlpsMonaco/proxy/util"
 )
 
-func TestClient(t *testing.T) {
-	b := []byte{1, 5, 0, 0}
-	t.Log(RecvSelectionMessage(b))
+func TestVersionMessageMethods(t *testing.T) {
+	t.Log(unsafe.Alignof(interface{}(1)))
+
+	var msg = Socks5_VersionMessage{
+		Ver:       5,
+		NumMethod: 0,
+		va:        [256]byte{},
+	}
+
+	t.Log(msg.GetMethods())
+	msg.SetMethod(0, 1, 2)
+	t.Log(msg.GetMethods())
+	t.Log(msg)
+
+	t.Log(util.ToBinary(&msg, int(2+msg.NumMethod)))
+
+	var b []byte = []byte{5, 7, 0, 1, 2, 3, 4, 5, 6}
+	var p *Socks5_VersionMessage
+	p = (*Socks5_VersionMessage)(unsafe.Pointer(*(*uintptr)(unsafe.Pointer(&b))))
+	t.Log(p.GetMethods())
+
 }
--- socks5/types.go
+++ socks5/types.go
@@ -1,21 +1,63 @@
 package socks5
 
+import (
+	"reflect"
+	"unsafe"
+
+	"github.com/AlpsMonaco/proxy/util"
+)
+
 const (
-	SOCKS5_METHOD_NO_AUTH uint8 = iota
+	SOCKS5_METHOD_NO_AUTH byte = iota
 	SOCKS5_METHOD_GSSAPI
 	SOCKS5_METHOD_USER_PASSWORD
+	SOCKS5_METHOD_NOT_SUPPORT
 )
 
 type Socks5_VersionMessage struct {
-	Ver       uint8
-	NumMethod uint8
-	Methods   [256]uint8
+	Ver       byte
+	NumMethod byte
+	va        [256]byte
+}
+
+func (vm *Socks5_VersionMessage) GetMethods() []byte {
+	if vm.NumMethod == 0 {
+		return nil
+	}
+
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(vm)) + unsafe.Offsetof(vm.va),
+		Len:  int(vm.NumMethod),
+		Cap:  int(vm.NumMethod),
+	}))
+}
+
+func (vm *Socks5_VersionMessage) SetMethod(methods ...byte) {
+	vm.NumMethod = byte(len(methods))
+	util.SetBytes(vm, int(unsafe.Offsetof(vm.va)), methods)
 }
 
 type Socks5_SelectionMessage struct {
-	Ver    uint8
-	Method uint8
+	Ver    byte
+	Method byte
 }
 
 type Socks5_RequestMessage struct {
+	Ver   byte
+	Cmd   byte
+	Rsv   byte
+	Atype byte
+	va    [256]byte
+}
+
+type Socks5_ResponseMessage struct {
+	Ver   byte
+	Rep   byte
+	Rsv   byte
+	Atype byte
+	va    [256]byte
+}
+
+type Socks5_UserPassVerify struct {
+	ProtocolVersion byte
 }
