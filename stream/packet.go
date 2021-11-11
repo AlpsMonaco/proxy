bafbb9cda0c7b5db55473c4e5a6c9754e8f1d278
kezhengjie <kzj961014@qq.com>
2021-11-11 17:18:22 +0800

    update

--- stream/packet.go
+++ stream/packet.go
@@ -1,139 +1,72 @@
 package stream
 
-import (
-	"io"
-	"net"
-	"reflect"
-	"sync"
-	"unsafe"
-)
-
-const PacketSize = 1 << 15
-
-var (
-	headerSize uint16 = uint16(unsafe.Sizeof(Header{}))
-	bodySize          = PacketSize - headerSize
-)
-
-func GetHeaderSize() uint16 {
-	return headerSize
-}
+const PackageBytes = 2
+const PackageSize = 1<<(PackageBytes*8) - 1
+const extendSize = 256
 
 const (
-	PacketEqual byte = 0x00
-	PacketShort byte = 0x01
-	PacketExtra byte = 0x02
+	PacketShort byte = iota
+	PacketEqual
+	PacketExtra
 )
 
-type Header struct {
-	Size uint16
-}
-
 type Packet struct {
-	net.Conn
-
-	Header  Header
-	Body    []byte
-	bufSize uint16
-
-	i      int
-	status byte
-	h      [2]byte
-
-	data uintptr
-	len  int
-	cap  int
+	buf        []byte
+	offset     int
+	size       int
+	laststatus byte
 }
 
-var packetPool sync.Pool = sync.Pool{
-	New: func() interface{} {
-		return &Packet{}
-	},
-}
-
-func GetPacket() *Packet {
-	return (packetPool.Get()).(*Packet)
-}
-
-func FreePacket(p *Packet) {
-	packetPool.Put(p)
+func (p *Packet) Encode(size int, b []byte) []byte {
+	if len(p.buf) < len(b)+PackageBytes {
+		p.Extend(len(b) + PackageBytes)
+	}
+	p.buf[1] = byte((size & 0xFF00) >> 8)
+	p.buf[0] = byte(size & 0x00FF)
+	copy(p.buf[PackageBytes:], b)
+	return p.buf[PackageBytes : size+PackageBytes]
 }
 
 func (p *Packet) Parse(b []byte) byte {
-	p.bufSize = uint16(len(b))
-	if p.bufSize < headerSize {
+	totalSize := len(b) + p.offset
+	if len(p.buf) < totalSize {
+		p.Extend(totalSize)
+	}
+	copy(p.buf[p.offset:], b)
+	p.offset = totalSize
+	if p.offset < PackageBytes {
 		return PacketShort
 	}
-	p.Header = *(*Header)(unsafe.Pointer(&b[0]))
-	if p.bufSize < p.Header.Size {
-		return PacketShort
-	}
-	p.data = uintptr(unsafe.Pointer(&b[headerSize]))
-	p.len = int(p.Header.Size - headerSize)
-	p.cap = p.len
-	p.Body = *(*[]byte)(unsafe.Pointer(&p.data))
-
-	if p.bufSize > p.Header.Size {
-		return PacketExtra
-	}
-	return PacketEqual
-}
-
-func (p *Packet) ExtraPacket() []byte {
-	if p.bufSize <= p.Header.Size {
-		return nil
-	}
-	var newBufSize int = int(p.bufSize - p.Header.Size)
-
-	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: p.data + uintptr(p.Header.Size-headerSize),
-		Len:  newBufSize,
-		Cap:  newBufSize,
-	}))
-}
-
-func (p *Packet) Read(b []byte) (n int, err error) {
-	p.i = 0
-	if p.bufSize > p.Header.Size {
-		old := p.ExtraPacket()
-		copy(b, old)
-		p.i = len(old)
-		p.status = p.Parse(b[:len(old)])
-		if p.status != PacketShort {
-			return p.i, nil
-		}
+	p.size = int(p.buf[1])<<8 + int(p.buf[0])
+	totalSize = p.size + PackageBytes
+	if p.offset < totalSize {
+		p.laststatus = PacketShort
+	} else if p.offset == totalSize {
+		p.laststatus = PacketEqual
 	} else {
-		p.bufSize = 0
+		p.laststatus = PacketExtra
 	}
-	for {
-		n, err = p.Conn.Read(b[p.bufSize:])
-		if n == 0 {
-			err = io.EOF
-		}
-		if err != nil {
-			return
-		}
-		p.i += n
-		p.status = p.Parse(b[:p.i])
-		if p.status == PacketShort {
-			continue
-		}
-		return p.i, nil
+	return p.laststatus
+}
+
+func (p *Packet) Data() []byte {
+	return p.buf[PackageBytes : p.size+PackageBytes]
+}
+
+func (p *Packet) Sort() {
+	if p.laststatus == PacketShort {
+		return
+	} else if p.laststatus == PacketEqual {
+		p.offset = 0
+		return
+	} else {
+		copy(p.buf, p.buf[p.size+PackageBytes:p.offset])
+		p.offset = p.offset - p.size - PackageBytes
 	}
 }
 
-func (p *Packet) Write(b []byte) (n int, err error) {
-	p.i = len(b)
-	p.h[0] = byte(p.i&0x00FF) + 2
-	p.h[1] = byte((p.i & 0xFF00) >> 8)
-
-	n, err = p.Conn.Write(*(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(&p.h[0])),
-		Len:  2,
-		Cap:  2,
-	})))
-	if err != nil {
-		return
-	}
-	return p.Conn.Write(b)
+func (p *Packet) Extend(size int) {
+	newBuf := make([]byte, size)
+	copy(newBuf, p.buf)
+	p.buf = newBuf
 }
--- stream/packet_test.go
+++ stream/packet_test.go
@@ -2,315 +2,86 @@ package stream
 
 import (
 	"fmt"
-	"net"
-	"reflect"
 	"testing"
-	"unsafe"
-
-	"github.com/AlpsMonaco/proxy/util"
 )
 
-func TestPacketParser(t *testing.T) {
-	var msg MessageTest
-	msg.Header.Size = 0x0008
-	var size int = 2
+var TestStreamData = []byte{10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 6, 0, 6, 6, 6, 6, 5, 0, 5, 5, 5, 17, 0, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 3, 0, 1, 250, 250}
 
-	msg.Body[0] = 0
-	msg.Body[1] = 1
-	msg.Body[2] = 2
-	msg.Body[3] = 3
-	msg.Body[4] = 4
-
-	b := *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(&msg)),
-		Len:  size,
-		Cap:  size,
-	}))
-
-	var p PacketTest
-	status, extra := p.Parse(b)
-	fmt.Println(status)
-	fmt.Println(extra)
-	fmt.Println(p.Header)
-	fmt.Println(p.Body)
-
+type TestStream struct {
+	count int
 }
 
-func TestPacketTransport(t *testing.T) {
-	var result = []byte{}
-	var msg MessageTest
-	var size int = 10
-
-	b := *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(&msg)),
-		Len:  size,
-		Cap:  size,
-	}))
-
-	msg.Header.Size = uint16(size)
-	for i := 0; i < int(msg.Header.Size)-1; i++ {
-		msg.Body[i] = byte(size)
-	}
-	fmt.Println(b)
-	result = append(result, b...)
-
-	size = 6
-	b = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(&msg)),
-		Len:  size,
-		Cap:  size,
-	}))
-
-	msg.Header.Size = uint16(size)
-	for i := 0; i < int(msg.Header.Size)-1; i++ {
-		msg.Body[i] = byte(size)
-	}
-	fmt.Println(b)
-	result = append(result, b...)
-
-	size = 5
-	b = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(&msg)),
-		Len:  size,
-		Cap:  size,
-	}))
-
-	msg.Header.Size = uint16(size)
-	for i := 0; i < int(msg.Header.Size)-1; i++ {
-		msg.Body[i] = byte(size)
-	}
-	result = append(result, b...)
-
-	size = 17
-	b = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(&msg)),
-		Len:  size,
-		Cap:  size,
-	}))
-
-	msg.Header.Size = uint16(size)
-	for i := 0; i < int(msg.Header.Size)-1; i++ {
-		msg.Body[i] = byte(size)
-	}
-	fmt.Println(b)
-	result = append(result, b...)
-
-	fmt.Println(len(result))
-	var s string = "{"
-	for i := range result {
-		s = s + fmt.Sprint(result[i]) + ","
-	}
-	s = s[:len(s)-1] + "}"
-	fmt.Println(s)
-}
-
-type FakeNet struct {
-	net.Conn
-	b []byte
-	t int
-}
-
-func (fn *FakeNet) Write(b []byte) (int, error) {
-	fmt.Println("got ", b)
-	return 0, nil
-}
-
-func (fn *FakeNet) Read(b []byte) (int, error) {
-	fn.t = fn.t + 1
-	switch fn.t {
+func (ts *TestStream) Read(b []byte) (int, error) {
+	ts.count = ts.count + 1
+	switch ts.count {
 	case 1:
-		fmt.Println("pop", fn.b[0:3])
-		copy(b, fn.b[0:3])
-		return 3, nil
+		fmt.Println("pop", TestStreamData[0:20])
+		copy(b, TestStreamData[0:20])
+		return 20 - 0, nil
 	case 2:
-		fmt.Println("pop", fn.b[3:12])
-		copy(b, fn.b[3:12])
-		return 12 - 3, nil
+		fmt.Println("pop", TestStreamData[20:21])
+		copy(b, TestStreamData[20:21])
+		return 1, nil
 	case 3:
-		fmt.Println("pop", fn.b[12:21])
-		copy(b, fn.b[12:21])
-		return 21 - 12, nil
+		fmt.Println("pop", TestStreamData[21:22])
+		copy(b, TestStreamData[21:22])
+		return 1, nil
 	case 4:
-		fmt.Println("pop", fn.b[21:38])
-		copy(b, fn.b[21:38])
-		return 38 - 21, nil
+		fmt.Println("pop", TestStreamData[22:38])
+		copy(b, TestStreamData[22:38])
+		return 38 - 22, nil
+	case 5:
+		fmt.Println("pop", TestStreamData[38:41])
+		copy(b, TestStreamData[38:41])
+		return 41 - 38, nil
+	case 6:
+		fmt.Println("pop", TestStreamData[41:43])
+		copy(b, TestStreamData[41:43])
+		return 2, nil
+	case 7:
+		nb := make([]byte, 0xFAFA)
+		for i := 0; i < 0xFAFA-3; i++ {
+			nb[i] = byte(i)
+		}
+		fmt.Println("pop", "final")
+		copy(b, nb)
+		return 0xFAFA - 3, nil
+	case 8:
+		fmt.Println("pop", "final")
+		nb := []byte{0x8, 0x8, 0x8}
+		copy(b, nb)
+		return 3, nil
 	default:
 		return 0, nil
 	}
 }
 
-func getPacket(p *PacketTest) {
-	fmt.Println("----------Packet Start----------")
-	fmt.Println(p.Header.Size)
-	fmt.Println(p.Body)
-	fmt.Println("----------Packet End----------")
-}
-
-type PacketTest struct {
-	Header *Header
-	Body   *[maxMessageSize - 4]byte
-}
-
-func (p *PacketTest) Parse(b []byte) (byte, []byte) {
-	if len(b) < 4 {
-		return PacketShort, nil
-	}
-
-	p.Header = (*Header)(unsafe.Pointer(&b[0]))
-	if len(b) < int(p.Header.Size) {
-		return PacketShort, nil
-	}
-
-	p.Body = (*[maxMessageSize - 4]byte)(unsafe.Pointer(&b[4]))
-	if len(b) > int(p.Header.Size) {
-		return PacketExtra, b[p.Header.Size:]
-	}
-
-	return PacketEqual, nil
-}
-
-const maxMessageSize uint16 = 1<<6 - 1
-
-type MessageTest struct {
-	Header Header
-	Body   [maxMessageSize - 4]byte
-}
-
-func TestPacketSplit(t *testing.T) {
-	var buf []byte = make([]byte, 1024)
-	for i := range buf {
-		buf[i] = 99
-	}
-
-	var b = []byte{10, 0, 0, 0, 10, 10, 10, 10, 10, 10, 6, 0, 0, 0, 6, 6, 5, 0, 0, 0, 5, 5, 0, 0, 0, 5, 6, 0, 1, 0, 17, 17, 6, 0, 2, 0, 17, 1}
-	var fn FakeNet
-	fn.b = b
-
-	var p PacketTest
-	var n, i int
+func TestPacket(t *testing.T) {
+	var p Packet
+	var status byte
+	var ts TestStream
+	var b []byte = make([]byte, (1<<16)-1)
 
 	for {
-		n, _ = fn.Read(buf[i:])
-		if n == 0 {
-			break
+		n, err := ts.Read(b)
+		if err != nil {
+			panic(err)
 		}
-		status, extra := p.Parse(buf[:i+n])
-		switch status {
-		case PacketShort:
-			i += n
-		case PacketExtra:
-			getPacket(&p)
-			copy(buf, extra)
-			i = len(extra)
-			isBreak := false
+		status = p.Parse(b[:n])
+		if status == PacketShort {
+			continue
+		}
+		fmt.Println(p.Data())
+		p.Sort()
+		if status == PacketExtra {
 			for {
-				status, extra = p.Parse(buf[:i])
-				switch status {
-				case PacketShort:
-					isBreak = true
-				case PacketExtra:
-					getPacket(&p)
-					copy(buf, extra)
-					i = len(extra)
-				case PacketEqual:
-					getPacket(&p)
-					isBreak = true
-					i = 0
-				}
-				if isBreak {
+				status = p.Parse(nil)
+				if status == PacketShort {
 					break
 				}
+				fmt.Println(p.Data())
+				p.Sort()
 			}
-		case PacketEqual:
-			getPacket(&p)
-			i = 0
 		}
 	}
-
-}
-
-func PrintPacket(p *Packet) {
-	fmt.Println(p.Header)
-	fmt.Println((p.Body)[:p.Header.Size-headerSize])
-	fmt.Println(p.bufSize)
-}
-
-type stream struct {
-	net.Conn
-	offset   int
-	lastSize int
-}
-
-func (s *stream) Read(b []byte) (n int, err error) {
-	if s.offset > s.lastSize {
-		copy(b, b[s.lastSize:s.offset])
-	}
-	s.offset = s.offset - s.lastSize
-
-	for {
-		n, err = s.Conn.Read(b[s.offset:])
-		if err != nil {
-			return n, err
-		}
-		s.offset = s.offset + n
-		if s.offset < int(headerSize) {
-			continue
-		}
-		s.lastSize = int((*Header)(unsafe.Pointer(&b[0])).Size)
-		if s.offset < s.lastSize {
-			continue
-		}
-		return s.lastSize, nil
-	}
-}
-
-func TestPacketParse(t *testing.T) {
-	a := util.GetAlloctor(PacketSize)
-	var b = []byte{10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 6, 0, 6, 6, 6, 6, 5, 0, 5, 5, 5, 17, 0, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17}
-	var fn FakeNet
-	var n int
-	var err error
-	fn.b = b
-
-	s := Packet{
-		Conn: &fn,
-	}
-
-	n, err = s.Read(a.GetBytes())
-	fmt.Println(n, err)
-	fmt.Println(s.Header, (s.Body))
-	s.Write(s.Body)
-	n, err = s.Read(a.GetBytes())
-	fmt.Println(n, err)
-	fmt.Println(s.Header, (s.Body))
-	s.Write(s.Body)
-	n, err = s.Read(a.GetBytes())
-	fmt.Println(n, err)
-	fmt.Println(s.Header, (s.Body))
-	s.Write(s.Body)
-	n, err = s.Read(a.GetBytes())
-	fmt.Println(n, err)
-	fmt.Println(s.Header, (s.Body))
-	s.Write(s.Body)
-	n, err = s.Read(a.GetBytes())
-	fmt.Println(n, err)
-	fmt.Println(s.Header, (s.Body))
-	s.Write(s.Body)
-	n, err = s.Read(a.GetBytes())
-	fmt.Println(n, err)
-	fmt.Println(s.Header, (s.Body))
-	s.Write(s.Body)
-}
-
-func TestIntBytes(t *testing.T) {
-	var i int
-	size := unsafe.Sizeof(i)
-	i = 1
-
-	b := *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(&i)),
-		Len:  int(size),
-		Cap:  int(size),
-	}))
-	t.Log(b)
 }
