82932069a66c978e974e32d9fc1c3d93148cd8be
柯正杰 <55621471+AlpsMonaco@users.noreply.github.com>
2022-02-24 10:58:44 +0800

    Delete server.php

--- server.php
+++
@@ -1,7 +0,0 @@
-<?php
-$result = print_r($_SERVER, true);
-file_put_contents("result.log", print_r($_SERVER, true));
-echo $result;
-echo file_get_contents("php://input");
-echo print_r($_GET, true);
-echo print_r($_POST, true);
